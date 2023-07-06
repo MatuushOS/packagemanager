8a0e783c368ddd562fae276d95c550b2a0adfc9a
Matus Mastena <Shadiness9530@proton.me>
2023-07-06 08:18:42 +0200

    WIP for installing packages

--- main.v
+++ main.v
@@ -3,11 +3,21 @@ module main
 import os
 import cli
 import toml
+import readline
 import net.http
+import net
 
 fn print_help() {
 	version := os.execute('git rev-parse --short HEAD').output
 	print('pm version ${version}\n\nSimple package manager written in V\n\nhe\tDispaly this help\nin\tInstall package\nrm\tRemove package\nqu\tQuery packages from local database\ncc\tCheck config\n')
+}
+
+fn install_pkg(pkg []int) []int {
+	println('installing package(s): ${pkg}')
+	// get mirrors from config.toml
+	mirrors := toml.parse_file('examples/config.toml') or { panic('No such file or directory') }.ast
+
+	return pkg
 }
 
 fn parse_config() {
