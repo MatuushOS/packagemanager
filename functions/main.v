2b9c6c78fb9fceb5abc92318fa32638bf7abccc4
Matus Mastena <Shadiness9530@proton.me>
2023-07-06 19:54:53 +0200

    Refactoring

--- functions/main.v
+++ functions/main.v
@@ -4,12 +4,12 @@ import os
 import toml
 import db.sqlite
 
-fn print_help() {
+pub fn print_help() {
 	version := os.execute('git rev-parse --short HEAD').output
 	print('pm version ${version}\n\nSimple package manager written in V\n\nhe\tDispaly this help\nin\tInstall package\nrm\tRemove package\nqu\tQuery packages from local database\ncc\tCheck config\n')
 }
 
-fn install_pkg(pkg string) string {
+pub fn install_pkg(pkg string) string {
 	println('installing package(s): ${pkg}')
 	// get mirrors from config.toml
 	parsefile := toml.parse_file('examples/config.toml') or { panic('No such file or directory') }.ast
@@ -18,13 +18,13 @@ fn install_pkg(pkg string) string {
 	return pkg
 }
 
-fn parse_config() {
+pub fn parse_config() {
 	file := toml.parse_file('examples/config.toml') or { panic('No such file or directory') }.ast
 	print('current config:\n\n${file}')
 	install_pkg('test')
 }
 
-fn mkrepo(name string, path string) {
+pub fn mkrepo(name string, path string) {
 	os.create('${path}/repo.db') or { panic('file exists') }
 	sqlite.connect('${path}/repo.db') or { panic('No such file or directory') }
 	println('Populating repo database with packages so PM can then see what packages are available')
