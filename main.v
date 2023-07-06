5614af12325b1b013096c876b7f0563f5266de88
Matus Mastena <Shadiness9530@proton.me>
2023-07-06 09:07:36 +0200

    Working on adding packages to the DB

--- main.v
+++ main.v
@@ -3,26 +3,34 @@ module main
 import os
 import cli
 import toml
-import readline
+import readline { read_line }
 import net.http
-import net
+import db.sqlite
 
 fn print_help() {
 	version := os.execute('git rev-parse --short HEAD').output
 	print('pm version ${version}\n\nSimple package manager written in V\n\nhe\tDispaly this help\nin\tInstall package\nrm\tRemove package\nqu\tQuery packages from local database\ncc\tCheck config\n')
 }
 
-fn install_pkg(pkg []int) []int {
+fn install_pkg(pkg string) string {
 	println('installing package(s): ${pkg}')
 	// get mirrors from config.toml
-	mirrors := toml.parse_file('examples/config.toml') or { panic('No such file or directory') }.ast
-
+	parsefile := toml.parse_file('examples/config.toml') or { panic('No such file or directory') }.ast
+	mirrors := parsefile.str()
+	println('${mirrors}')
 	return pkg
 }
 
 fn parse_config() {
 	file := toml.parse_file('examples/config.toml') or { panic('No such file or directory') }.ast
 	print('current config:\n\n${file}')
+	install_pkg('test')
+}
+
+fn mkrepo(name string, path string) {
+	os.create('${path}/repo.db') or { panic('file exists') }
+	sqlite.connect('${path}/repo.db') or { panic('No such file or directory') }
+	println('Populating repo database with packages so PM can then see what packages are available')
 }
 
 fn main() {
@@ -59,6 +67,16 @@ fn main() {
 				name: 'cc'
 				execute: fn (cmd cli.Command) ! {
 					parse_config()
+					return
+				}
+			},
+			cli.Command{
+				name: 'mkrepo'
+				description: 'Create database for repository'
+				execute: fn (cmd cli.Command) ! {
+					name := read_line("What's the name of your repo?: ")
+					path := read_line("What's the path where you have packages?: ")
+					mkrepo('${name}', '${path}')
 					return
 				}
 			},
